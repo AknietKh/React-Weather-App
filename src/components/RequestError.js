23f523aaed6854ccdc8fa2e3538f0a078a82a9f9
Akniet.Kh <akniet.khakimov@mail.ru>
2020-01-10 22:04:11 +0600

    Компонент теперь показывает ошибку, если не было получены данные при геолокации

--- src/components/RequestError.js
+++ src/components/RequestError.js
@@ -3,10 +3,11 @@ import '../App.css';
 
 function RequestError(props) {
  const errStatus = +props.errStatus;
+ const errGeo = props.errGeo;
   return (
     <div className="weather-display">
       <div className="weather-display__error">
-        <h2>Произошла ошибка!</h2>
+        <h2>Ошибка!</h2>
         {
           (errStatus >= 400 && errStatus < 500)  &&  
           <p>
@@ -17,6 +18,15 @@ function RequestError(props) {
         {
           (errStatus >= 500 && errStatus < 600)  && 
           <p>Сервер не отвечает, ошибка {errStatus}. Попробуйте позже.</p>
+        }
+        {
+          errGeo && 
+          <p>
+            Произошла ошибка при запросе данных о местоположении.<br/>
+            Попробуйте повторить позже или можете ввести название города
+            в строку поиска.<br/><hr/>
+            Ошибка: {errGeo.code}. {errGeo.message}
+          </p>
         }
       </div>
     </div>
