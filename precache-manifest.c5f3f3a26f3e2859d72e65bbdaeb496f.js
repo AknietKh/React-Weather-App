92da74e576df840fc3f11f343af9a9863887ff3a
Akniet.Kh <akniet.khakimov@mail.ru>
2020-01-11 04:57:02 +0600

    Updates

---
+++ precache-manifest.c5f3f3a26f3e2859d72e65bbdaeb496f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54443ab3b43a25358fd7679cbfd9978b",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "faeb495efa519dadeabb",
+    "url": "/react-weather-app/static/css/main.1a3cb91b.chunk.css"
+  },
+  {
+    "revision": "50b6894a7fd69ddf0047",
+    "url": "/react-weather-app/static/js/2.58bd8978.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-weather-app/static/js/2.58bd8978.chunk.js.LICENSE"
+  },
+  {
+    "revision": "faeb495efa519dadeabb",
+    "url": "/react-weather-app/static/js/main.46ef43d9.chunk.js"
+  },
+  {
+    "revision": "f71b421efae2266b3d47",
+    "url": "/react-weather-app/static/js/runtime-main.52d58785.js"
+  }
+]);
